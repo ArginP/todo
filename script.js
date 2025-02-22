6c30e9bd9ded6d842a245bd6503d1527726c1908
ArginP <n7toman@gmail.com>
2025-02-22 22:08:39 +0300

    fix: befriended search logic with add task logic, code optimisation and cleanup

--- script.js
+++ script.js
@@ -12,16 +12,17 @@ let filteredTasks = [];
 // --- логика создания новой таски ---
 const addTask = () => {
     if (addTaskInput.value.trim()) { // проверяет что поле текста таски не пустое
-        const task = { // задаем объект таски
+        const task = { // задает объект таски
             id: Date.now(), // костыль псевдоуникального ID
             description: addTaskInput.value.trim(), // текст таски без пробелов с концов
             completed: false, // по-умолчанию не выполнено
             createdAt: getTimeFormat(new Date()), // дата создания таски
         }
-        tasks.push(task); // добавляем таску в конец массива тасок
-        setLocalStorage(tasks); // записываем этот массив в localStorage
-        addTaskInput.value = ''; // очищаем поле ввода
-        renderTasks(); // отрисовывает текущий массив тасок
+        tasks.push(task); // добавляет таску в конец массива тасок
+        setLocalStorage(tasks); // записывает этот массив в localStorage
+        addTaskInput.value = ''; // очищает поле ввода
+
+        whichRenderTasks();
     }
 }
 
@@ -33,18 +34,16 @@ addTaskInput.addEventListener('keydown', (event) => {
     }
 })
 
-// --- обработчик ввода в поле поиска и логика фильтрования тасок под поиск ---
-searchInput.addEventListener('input', (event) => { // обработчик по вводу в поле поиска
-    // const searchValue = event.target.value.toLowerCase().trim(); // введенное значение
-
+// --- обработчик ввода в поле поиска ---
+searchInput.addEventListener('input', (event) => {
     debouncedSearch(event.target.value.toLowerCase().trim());
 })
 
 // --- поиск без задержки, срабатывает при нажатии кнопок ---
 const search = (searchValue) => {
     filteredTasks = tasks.filter((t) => {
-        // отфильтровываем массив тасок, и передаем подходящие значения в массив filteredTasks, хранящийся в памяти вкладки
-        return t.description.toLowerCase().includes(searchValue) // возвращаем таски, подходящие под ключ поиска
+        // отфильтровывает массив тасок, и передает подходящие значения в массив filteredTasks, хранящийся в памяти вкладки
+        return t.description.toLowerCase().includes(searchValue) // возвращает таски, подходящие под ключ поиска
     });
 
     renderFilteredTasks(); // отрисовывает отфильтрованные таски
@@ -52,19 +51,14 @@ const search = (searchValue) => {
 
 // --- дебаунс поиск, срабатывает при вводе в поле поиска ---
 const debouncedSearch = debounce((searchValue) => {
-    filteredTasks = tasks.filter((t) => {
-        // отфильтровываем массив тасок, и передаем подходящие значения в массив filteredTasks, хранящийся в памяти вкладки
-        return t.description.toLowerCase().includes(searchValue) // возвращаем таски, подходящие под ключ поиска
-    });
-
-    renderFilteredTasks(); // отрисовывает отфильтрованные таски
+    search(searchValue);
 });
 
 
 // --- генерация динамической HTML разметки под таски ---
 const createTaskLayout = (task) => {
     const taskItem = document.importNode(taskTemplate.content, true);
-    // получаем HTML нод из темплейта, true = с вложениями
+    // получает HTML нод из темплейта, true = с вложениями
 
     const taskCheckbox = taskItem.querySelector('[data-task-checkbox]');
     taskCheckbox.checked = task.completed; // чекбокс будет соответствовать значению completed в объекте таски
@@ -80,7 +74,7 @@ const createTaskLayout = (task) => {
     taskDelete.disabled = !task.completed;
     // кнопка удалить заблокирована, пока таска не помечена как выполненная в значении completed объекта таски
 
-    // отслеживаем ивент изменения состояния checked галочки
+    // отслеживает ивент изменения состояния checked галочки
     taskCheckbox.addEventListener('change', (event) => {
         tasks = tasks.map((t) => {
             // метод, который применяет колбек функцию к элементам массива, и создает новый, измененный, массив
@@ -91,29 +85,21 @@ const createTaskLayout = (task) => {
         });
         setLocalStorage(tasks); // записывает таски в localStorage
 
-        if (searchInput.value.trim()) { // если в окне поиска что-то введено
-            search(searchInput.value.toLowerCase().trim()); // отрисовывает таски по фильтру
-        } else {
-            renderTasks(); // отрисовывает все таски
-        }
+        whichRenderTasks();
     })
 
-    // отслеживаем клик по кнопке удалить таску
+    // отслеживает клик по кнопке удалить таску
     taskDelete.addEventListener('click', () => {
         tasks = tasks.filter((t) => {
             // метод, который фильтрует массива на основании параметров, заданных колбеком
-            if (t.id !== task.id) { // фильтруем по ID т.о., чтобы фильтр прошли все таски, кроме удаляемой
+            if (t.id !== task.id) { // фильтрует по ID т.о., чтобы фильтр прошли все таски, кроме удаляемой
                 return t; // возвращает все элементы массива, ID которых НЕ СООТВЕТСТВУЕТ ID удаляемой таски
             }
 
         });
         setLocalStorage(tasks); // записывает таски в localStorage
 
-        if (searchInput.value.trim()) { // если в окне поиска что-то введено
-            search(searchInput.value.toLowerCase().trim()); // отрисовывает таски по фильтру
-        } else {
-            renderTasks(); // отрисовывает все таски
-        }
+        whichRenderTasks();
     })
 
     return taskItem;
@@ -134,8 +120,6 @@ const renderTasks = () => {
     })
 }
 
-renderTasks(); // вызываем функцию рендера, чтобы при первоначальной загрузке страницы таски отрисовались
-
 // --- отрисовка отфильтрованных тасок ---
 const renderFilteredTasks = () => {
     tasksContainer.innerHTML = ''; // очищает контейнер тасок
@@ -150,3 +134,14 @@ const renderFilteredTasks = () => {
         tasksContainer.append(taskItem) // выводит таску на страницу
     })
 }
+
+// --- какой рендер использовать, для всех тасок, или фильтрованных? ---
+const whichRenderTasks = () => {
+    if (searchInput.value.trim()) { // если в окне поиска что-то введено
+        search(searchInput.value.toLowerCase().trim()); // отрисовывает таски по фильтру
+    } else {
+        renderTasks(); // отрисовывает все таски
+    }
+}
+
+whichRenderTasks(); // вызывает функцию рендера, чтобы при первоначальной загрузке страницы таски отрисовались
